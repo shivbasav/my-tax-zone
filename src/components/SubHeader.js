9393bc1e52b44b67a8638b35d045bc5d8d15c95f
Shivbasav Nandargi <shivagn119@gmail.com>
2020-10-01 23:03:32 +0530

    new sub header

--- src/components/SubHeader.js
+++ src/components/SubHeader.js
@@ -1,21 +1,310 @@
-import { Link } from "gatsby"
+// // import { Link } from "gatsby"
+// // import React from "react"
+// // import styled from "styled-components"
+// // import { Text } from "./elements/Text"
+
+// // const SubHeader = () => (
+// //   <nav className="navbar is-white">
+// //     <div className="navbar-item has-dropdown is-hoverable">
+// //       <a className="navbar-link" to="/">
+// //         Docs
+// //       </a>
+// //       <div className="navbar-dropdown is-boxed">
+// //         <Link to="page-2/" className="navbar-item">
+// //           sdfsfds
+// //         </Link>
+// //       </div>
+// //     </div>
+// //   </nav>
+// // )
+
+// // export default SubHeader
+// import React from "react"
+
+// import styled from "styled-components"
+// import { Link } from "gatsby"
+
+// const NavBar = styled.nav`
+//   &&& {
+//     position: fixed;
+//     width: 100%;
+//     z-index: 1000;
+//     height: 4rem;
+//     .navbar-item.has-dropdown {
+//       align-items: center;
+//     }
+
+//     .button {
+//       height: 2.8em;
+//     }
+//     .navbar-item img {
+//       max-height: 3rem;
+//     }
+//     @media screen and (min-width: 1330px) {
+//       padding: 0rem 0rem;
+//     }
+//     @media screen and (min-width: 1370px) {
+//       padding: 0rem 1rem;
+//     }
+//     @media screen and (min-width: 1430px) {
+//       padding: 0rem 2rem;
+//     }
+//     @media screen and (min-width: 1470px) {
+//       padding: 0rem 4rem;
+//     }
+//     @media screen and (min-width: 1500px) {
+//       padding: 0rem 7rem;
+//     }
+//     @media screen and (min-width: 1550px) {
+//       padding: 0rem 9rem;
+//     }
+//     @media screen and (min-width: 1650px) {
+//       padding: 0rem 11rem;
+//     }
+//     @media screen and (min-width: 1700px) {
+//       padding: 0rem 13rem;
+//     }
+//     @media screen and (min-width: 2000px) {
+//       padding: 0rem 14rem;
+//     }
+//     /* padding: 0rem 24px; */
+//   }
+// `
+
+// const NavbarLink = styled.a``
+// const ImageWrapper = styled.span`
+//   &&& {
+//     width: 2.5rem;
+//     img {
+//       height: 2.5rem;
+//       border-radius: 30px;
+//       width: 2.5rem;
+//     }
+//   }
+// `
+
+// const SubHeader = ({ history }) => {
+//   return (
+//     <NavBar
+//       className="navbar is-light"
+//       role="navigation"
+//       aria-label="main navigation"
+//     >
+//       <div id="navbarBasicExample" className="navbar-menu">
+//         <div className="navbar-end" />
+//         <div>skjfkjs</div>
+//         <div className="navbar-end">
+//           <div className="navbar-item has-dropdown is-hoverable">
+//             <NavbarLink className="navbar-link">
+//               <p color="white" size="14px">
+//                 Add a Place
+//               </p>
+//             </NavbarLink>
+
+//             <div className="navbar-dropdown">
+//               <div className="navbar-item navbar-brand">
+//                 <Link to="/add-place">
+//                   <p size="smaller" color="darkGrey" weight="semibold">
+//                     Add a Place
+//                   </p>
+//                 </Link>
+//               </div>
+//               <div className="navbar-item">
+//                 <Link to="/search-place-for-claim">
+//                   <p size="smaller" color="darkGrey" weight="semibold">
+//                     Claim a Place
+//                   </p>
+//                 </Link>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+
+//         <div className="navbar-end">
+//           <div className="navbar-item has-dropdown is-hoverable">
+//             <NavbarLink className="navbar-link">
+//               <p color="white" size="smaller">
+//                 Add a Place
+//               </p>
+//             </NavbarLink>
+
+//             <div className="navbar-dropdown">
+//               <div className="navbar-item navbar-brand">
+//                 <Link to="/add-place">
+//                   <p size="smaller" color="darkGrey" weight="semibold">
+//                     Add a Place
+//                   </p>
+//                 </Link>
+//               </div>
+//               <div className="navbar-item">
+//                 <Link to="/search-place-for-claim">
+//                   <p size="smaller" color="darkGrey" weight="semibold">
+//                     Claim a Place
+//                   </p>
+//                 </Link>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+
+//         <div className="navbar-end">
+//           <div className="navbar-item has-dropdown is-hoverable">
+//             <NavbarLink className="navbar-link">
+//               <p color="white" size="smaller">
+//                 Add a Place
+//               </p>
+//             </NavbarLink>
+
+//             <div className="navbar-dropdown">
+//               <div className="navbar-item navbar-brand">
+//                 <Link to="/add-place">
+//                   <p size="smaller" color="darkGrey" weight="semibold">
+//                     Add a Place
+//                   </p>
+//                 </Link>
+//               </div>
+//               <div className="navbar-item">
+//                 <Link to="/search-place-for-claim">
+//                   <p size="smaller" color="darkGrey" weight="semibold">
+//                     Claim a Place
+//                   </p>
+//                 </Link>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+
+//         <div className="navbar-end">
+//           <div className="navbar-item has-dropdown is-hoverable">
+//             <NavbarLink className="navbar-link">
+//               <p color="black" size="smaller">
+//                 Add a Place
+//               </p>
+//             </NavbarLink>
+
+//             <div className="navbar-dropdown">
+//               <div className="navbar-item navbar-brand">
+//                 <Link to="/add-place">
+//                   <p size="smaller" color="darkGrey" weight="semibold">
+//                     Add a Place
+//                   </p>
+//                 </Link>
+//               </div>
+//               <div className="navbar-item">
+//                 <Link to="/search-place-for-claim">
+//                   <p size="smaller" color="darkGrey" weight="semibold">
+//                     Claim a Place
+//                   </p>
+//                 </Link>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </NavBar>
+//   )
+// }
+
+// export default SubHeader
 import React from "react"
 import styled from "styled-components"
-import { Text } from "./elements/Text"
+import { Link } from "gatsby"
 
-const SubHeader = () => (
-  <nav className="navbar is-light">
-    <div className="navbar-item has-dropdown is-hoverable">
-      <a className="navbar-link" to="/">
-        Docs
-      </a>
-      <div className="navbar-dropdown is-boxed">
-        <Link to="page-2/" className="navbar-item">
-          sdfsfds
-        </Link>
-      </div>
-    </div>
-  </nav>
-)
+const NavbarLink = styled.a``
+const Container = styled.div`
+  padding-left: 1rem;
+  width: 100%;
+  z-index: 100;
+  position: sticky;
+  top: 0rem;
+  -moz-box-shadow: 0px 0px 7px rgba(0, 0, 0, 0.15);
+  -webkit-box-shadow: 0px 0px 7px rgba(0, 0, 0, 0.15);
+  box-shadow: 0px 0px 7px rgba(0, 0, 0, 0.15);
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 0rem auto;
+  max-width: 1330px;
+  padding: 0 20px;
+`
+
+const Item = styled.div`
+  margin: 0rem 0.8rem;
+  padding: 0.4rem 0.2rem;
+  position: relative;
+  top: 2px;
+  border-bottom: 2px solid
+    ${({ theme: { primaryColor }, isActive }) =>
+      isActive ? primaryColor : "transparent"};
+`
+
+// const FlexStyle = styled(Flex).attrs({ justify: "flex-start" })``
+
+// const TextStyle = styled(Text).attrs({ size: "tiny", color: "dark" })`
+//   padding-left: 0.5rem;
+//   &&& {
+//     :hover {
+//       color: ${({ theme: { primaryColor } }) => primaryColor};
+//     }
+//   }
+// `
+
+const SubHeader = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <div className="navbar-end">
+          <div className="navbar-item has-dropdown is-hoverable">
+            <NavbarLink className="navbar-link">
+              <p color="white" size="14px">
+                Add a Place
+              </p>
+            </NavbarLink>
+
+            <div className="navbar-dropdown">
+              <div className="navbar-item navbar-brand">
+                <Link to="/add-place">
+                  <p size="smaller" color="darkGrey" weight="semibold">
+                    Add a Place
+                  </p>
+                </Link>
+              </div>
+              <div className="navbar-item">
+                <Link to="/search-place-for-claim">
+                  <p size="smaller" color="darkGrey" weight="semibold">
+                    Claim a Place
+                  </p>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Item>
+          <p>Find An Item</p>
+        </Item>
+
+        <Item>
+          <p>Order Food</p>
+        </Item>
+
+        <Item>
+          <p>Discover Events</p>
+        </Item>
+
+        <Item>
+          <p>Spot and Share An Item</p>
+        </Item>
+
+        <Item>
+          <p>Write a Review</p>
+        </Item>
+      </Wrapper>
+      {/* <Breadcrumbs /> */}
+    </Container>
+  )
+}
 
 export default SubHeader
--- src/components/header.js
+++ src/components/header.js
@@ -3,48 +3,50 @@ import React from "react"
 import styled from "styled-components"
 
 const Header = () => (
-  <nav
-    className="navbar is-light"
-    rol="navigation"
-    aria-label="main navigation"
-  >
-    <div className="navbar-brand">
-      <Link className="navbar-item" to="/">
-        <span>
-          <p
-            className="has-text-info has-text-weight-semibold is-size-2"
-            size="big"
-            weight="bold"
-            letterSpacing="loose"
-          >
-            MyTaxZone
-          </p>
-          <p className="is-size-4 has-text-info">Consulting and Compliance</p>
-        </span>
-      </Link>
-      <a
-        role="button"
-        className="navbar-burger burger"
-        aria-label="menu"
-        aria-expanded="false"
-        data-target="navbarBasicExample"
-      >
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-        <span aria-hidden="true"></span>
-      </a>
-    </div>
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">Log in</a>
+  <>
+    <nav
+      className="navbar is-white"
+      rol="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <Link className="navbar-item" to="/">
+          <span>
+            <p
+              className="has-text-info has-text-weight-semibold is-size-3"
+              size="big"
+              weight="bold"
+              letterSpacing="loose"
+            >
+              MyTaxZone
+            </p>
+            <p className="is-size-5 has-text-info">Consulting and Compliance</p>
+          </span>
+        </Link>
+        <a
+          role="button"
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">Log in</a>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </>
 )
 
 export default Header
